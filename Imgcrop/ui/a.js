ed89f9a391aa3689d4295f25ca379fba2ac07e0c
Pratyush, MISHRA <Pratyushmishra@u.nus.edu>
2019-02-09 14:06:54 +0800

    Update a.js

--- Imgcrop/ui/a.js
+++ Imgcrop/ui/a.js
@@ -61,7 +61,7 @@ request(options, function (error, response, body) {
   console.log(body);
 });
     
- //   var image = document.getElementById("canvas1").toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+    var image = document.getElementById("canvas1").toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
 
 
 window.location.href=image;
